9ce33af2b63fcfd839d47ea7cfc04c1e95910957
thunder913 <andon_00@abv.bg>
2022-10-17 21:33:51 +0300

    Start integrating a pagination on page

---
+++ StackOverflow_Statistics/Dtos/UsersCommentsCountDto.cs
@@ -0,0 +1,9 @@
+﻿namespace StackOverflow_Statistics.Dtos
+{
+    public class UsersCommentsCountDto
+    {
+        public string DisplayName { get; set; }
+        public long CommentCount { get; set; }
+        public long Score { get; set; }
+    }
+}
--- StackOverflow_Statistics/ViewModels/UsersCommentsCountViewModel.cs
+++ StackOverflow_Statistics/ViewModels/UsersCommentsCountViewModel.cs
@@ -1,6 +1,149 @@
-﻿namespace StackOverflow_Statistics.ViewModels
+﻿using StackOverflow_Statistics.Common;
+using StackOverflow_Statistics.Dtos;
+using StackOverflow_Statistics.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace StackOverflow_Statistics.ViewModels
 {
-    public class UsersCommentsCountViewModel
+    public class UsersCommentsCountViewModel : ObservableObject
     {
+        //TODO INTEGRATE ONE PAGE WITH PAGINATIONS
+        public ObservableCollection<UsersCommentsCountDto> Items { get; set; }
+        private string _countString;
+        public string CountString
+        {
+            get => _countString;
+            set
+            {
+                _countString = value;
+                OnPropertyChanged();
+            }
+        }
+        public UsersCommentsCountViewModel(IUserService userService)
+        {
+            List<object> myList = GetData();
+            this.userService = userService;
+            CountString = 1 + " of " + this.userService.GetUsersCount();
+        }
+
+        int pageIndex = 1;
+        private int numberOfRecPerPage;
+        private readonly IUserService userService;
+
+        private enum PagingMode { First = 1, Next = 2, Previous = 3, Last = 4, PageCountChange = 5 };
+        
+        private List<object> GetData()
+        {
+            List<object> genericList = new List<object>();
+            Student studentObj;
+            Random randomObj = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                studentObj = new Student();
+                studentObj.FirstName = "First " + i;
+                studentObj.MiddleName = "Middle " + i;
+                studentObj.LastName = "Last " + i;
+                studentObj.Age = (uint)randomObj.Next(1, 100);
+
+                genericList.Add(studentObj);
+            }
+            return genericList;
+        }
+
+        private void Navigate(int mode)
+        {
+            int count;
+            switch (mode)
+            {
+                case (int)PagingMode.Next:
+                    btnPrev.IsEnabled = true;
+                    btnFirst.IsEnabled = true;
+                    if (myList.Count >= (pageIndex * numberOfRecPerPage))
+                    {
+                        if (myList.Skip(pageIndex *
+                        numberOfRecPerPage).Take(numberOfRecPerPage).Count() == 0)
+                        {
+                            dataGrid.ItemsSource = null;
+                            dataGrid.ItemsSource = myList.Skip((pageIndex *
+                            numberOfRecPerPage) - numberOfRecPerPage).Take(numberOfRecPerPage);
+                            count = (pageIndex * numberOfRecPerPage) +
+                            (myList.Skip(pageIndex *
+                            numberOfRecPerPage).Take(numberOfRecPerPage)).Count();
+                        }
+                        else
+                        {
+                            dataGrid.ItemsSource = null;
+                            dataGrid.ItemsSource = myList.Skip(pageIndex *
+                            numberOfRecPerPage).Take(numberOfRecPerPage);
+                            count = (pageIndex * numberOfRecPerPage) +
+                            (myList.Skip(pageIndex *
+                            numberOfRecPerPage).Take(numberOfRecPerPage)).Count();
+                            pageIndex++;
+                        }
+
+                        lblpageInformation.Content = count + " of " + myList.Count;
+                    }
+
+                    else
+                    {
+                        btnNext.IsEnabled = false;
+                        btnLast.IsEnabled = false;
+                    }
+
+                    break;
+                case (int)PagingMode.Previous:
+                    btnNext.IsEnabled = true;
+                    btnLast.IsEnabled = true;
+                    if (pageIndex > 1)
+                    {
+                        pageIndex -= 1;
+                        dataGrid.ItemsSource = null;
+                        if (pageIndex == 1)
+                        {
+                            dataGrid.ItemsSource = myList.Take(numberOfRecPerPage);
+                            count = myList.Take(numberOfRecPerPage).Count();
+                            lblpageInformation.Content = count + " of " + myList.Count;
+                        }
+                        else
+                        {
+                            dataGrid.ItemsSource = myList.Skip
+                            (pageIndex * numberOfRecPerPage).Take(numberOfRecPerPage);
+                            count = Math.Min(pageIndex * numberOfRecPerPage, myList.Count);
+                            lblpageInformation.Content = count + " of " + myList.Count;
+                        }
+                    }
+                    else
+                    {
+                        btnPrev.IsEnabled = false;
+                        btnFirst.IsEnabled = false;
+                    }
+                    break;
+
+                case (int)PagingMode.First:
+                    pageIndex = 2;
+                    Navigate((int)PagingMode.Previous);
+                    break;
+                case (int)PagingMode.Last:
+                    pageIndex = (myList.Count / numberOfRecPerPage);
+                    Navigate((int)PagingMode.Next);
+                    break;
+
+                case (int)PagingMode.PageCountChange:
+                    pageIndex = 1;
+                    numberOfRecPerPage = Convert.ToInt32(cbNumberOfRecords.SelectedItem);
+                    dataGrid.ItemsSource = null;
+                    dataGrid.ItemsSource = myList.Take(numberOfRecPerPage);
+                    count = (myList.Take(numberOfRecPerPage)).Count();
+                    lblpageInformation.Content = count + " of " + myList.Count;
+                    btnNext.IsEnabled = true;
+                    btnLast.IsEnabled = true;
+                    btnPrev.IsEnabled = true;
+                    btnFirst.IsEnabled = true;
+                    break;
+            }
+        }
     }
 }
