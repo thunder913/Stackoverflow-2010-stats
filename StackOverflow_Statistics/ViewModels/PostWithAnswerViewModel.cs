9f00e6a1e6e511e9c4a31c373c4f1de05d4d53c0
thunder913 <andon_00@abv.bg>
2022-10-23 21:53:31 +0300

    Fix naming of pages and minor improvements

---
+++ StackOverflow_Statistics/ViewModels/PostWithAnswerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using StackOverflow_Statistics.Common;
+
+namespace StackOverflow_Statistics.ViewModels
+{
+    public class PostWithAnswerViewModel : ObservableObject
+    {
+        public PostWithAnswerViewModel()
+        {
+            HtmlString = "<h1>Test Message</h1>";
+        }
+
+        public string htmlString { get; set; }
+        public string HtmlString
+        {
+            get => htmlString;
+            set
+            {
+                htmlString = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
